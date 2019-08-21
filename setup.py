414295ccfe94329ff5ec473b3b071ceef6ed2c50
Dolf Andringa <dolfandringa@gmail.com>
2019-08-21 18:07:19 +0800

    Bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='pycayennelpp',
-    version='1.3.0',
+    version='1.4.0',
     python_requires='>=3.4',
     description='Encoder and Decoder for CayenneLLP',
     long_description=readme(),
